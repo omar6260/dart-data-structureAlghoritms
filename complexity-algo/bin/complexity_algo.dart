b3046908aa81806f458b187dd7205109c05d5027
Oumar Fall <foumar404@gmail.com>
2022-05-14 12:58:13 +0100

    adding to complexity alghoritms

---
+++ complexity-algo/bin/complexity_algo.dart
@@ -0,0 +1,5 @@
+import 'package:complexity_algo/complexity_algo.dart' as complexity_algo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${complexity_algo.calculate()}!');
+}
---
+++ complexity-algo/test/complexity_algo_test.dart
@@ -0,0 +1,8 @@
+import 'package:complexity_algo/complexity_algo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
