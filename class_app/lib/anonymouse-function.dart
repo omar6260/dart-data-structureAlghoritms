36b1d559a7ef9d97b792a9d00e1596236b295187
Oumar Fall <foumar404@gmail.com>
2022-11-27 23:34:40 +0100

    add anonymouse function

---
+++ class_app/lib/anonymouse-function.dart
@@ -0,0 +1,13 @@
+void main() {
+  assigningFunctionToVariable();
+}
+
+void assigningFunctionToVariable() {
+  int number = 4;
+  String greeting = 'hello';
+  bool isHungry = true;
+
+  Function multiply = (int a, int b) {
+    return a * b;
+  };
+}
